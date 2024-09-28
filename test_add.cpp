ef123548c1f3744009bb623721ab5cdcfce44359
lainfag <biggusvalerius@gmail.com>
2024-09-28 19:27:56 +0300

    add tests

---
+++ test_add.cpp
@@ -0,0 +1,9 @@
+#include<gtest/gtest.h>
+
+#include "my_add.hpp"
+
+TEST(Add, Simple){
+    EXPECT_EQ(my_add(2, 2), 4);
+    EXPECT_EQ(my_add(1, 2), my_add(2, 1));
+    EXPECT_EQ(my_add(-100, 30), -70);
+}

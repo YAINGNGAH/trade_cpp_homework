2756eab654a0564c4d08301f7a9f0dee84398ffa
lainfag <biggusvalerius@gmail.com>
2024-09-28 20:10:04 +0300

    Бенчмарк

---
+++ bench_add.cpp
@@ -0,0 +1,15 @@
+#include <benchmark/benchmark.h>
+
+#include "my_add.hpp"
+
+static void BM_Add(benchmark::State& state) {
+  for (auto _ : state) {
+    my_add(std::rand(), std::rand());
+  }
+}
+
+
+
+BENCHMARK(BM_Add);
+
+BENCHMARK_MAIN();
